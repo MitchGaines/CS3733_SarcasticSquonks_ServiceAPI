8776eadcfb1f0c50db54f910e823c3a2f1f17f9a
Mitch <gainesmitch2@gmail.com>
2018-04-15 21:36:08 -0400

    init project setup, doesn't build for obvious reasons

---
+++ src/main/java/edu/wpi/cs3733d18/teamS/Main.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733d18.teamS;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    private static Stage primary_stage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static Object switchScenes(String title, String fxml_name) {
+    }
+
+    @Override
+    public void start(Stage primary_stage) throws Exception {
+
+    }
+}
